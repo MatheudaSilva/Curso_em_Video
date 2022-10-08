96d3c22a45c45edd35f22719f91f473c4575cbc7
Matheus da Silva <matheus4589silva@gmail.com>
2022-10-07 22:31:51 -0300

    first commit

---
+++ JS - Curso/Aula11/Ex008.js
@@ -0,0 +1,6 @@
+var vel = 49
+console.log(`A velocidade do seu carro é ${vel} Km/h`);
+if(vel > 50){
+    console.log('Você foi multado, por ultrapassar o limite de velocidade.')
+}
+console.log('Dirija sempre usando cinto de segurança!') 
