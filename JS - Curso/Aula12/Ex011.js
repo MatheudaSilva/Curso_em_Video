0a392d4b285ffa2c37daf73cb9de1e7e84c194c7
Matheus da Silva <matheus4589silva@gmail.com>
2022-10-10 22:02:45 -0300

    first commit

--- JS - Curso/Aula12/Ex011.js
+++ JS - Curso/Aula12/Ex011.js
@@ -1,4 +1,11 @@
-var idade = 22
-if(idade < 18){
-    console.log
+var idade = 43
+console.log (`Você tem ${idade} anos`)
+if(idade < 16 ){
+    console.log('Não pode votar')
+}
+else if(idade < 18 || idade > 66 ){
+    console.log('O voto é opcional')
+}
+else{
+    console.log('O voto é obrígatório')
 }
