46504ca6b6a1fd86bdef004ac7fa5a4c495d782f
Matheus da Silva <matheus4589silva@gmail.com>
2022-10-15 15:29:12 -0300

    first commit

---
+++ JS - Curso/Aula13/ambiente.js
@@ -0,0 +1,27 @@
+var c = 1
+do
+{
+    console.log(`Serei escrito ${c} vezes`)
+    c++
+} 
+while(c <= 6)
+    
+/*var c = 1
+while(c <= 500)
+{
+    console.log(`Serei escrito ${c} vezes`)
+    c++
+}
+*/
+
+
+
+/*
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+*/
